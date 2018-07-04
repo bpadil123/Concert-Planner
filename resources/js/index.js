6f7090bc0cd54661239c3ff06dafea2438321701
Inna <designbyinna@gmail.com>
2018-07-04 08:15:23 -0600

    last fm url accept search results that have multiple words

--- resources/js/index.js
+++ resources/js/index.js
@@ -11,13 +11,15 @@ var searchTerm;
 
 function getLastFm() {
     searchTerm = $("#search-input").val().trim();
-    url = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + searchTerm + "&api_key=a63e099b17dfc041516f78ab8c3e3d5c&format=json"
+    var replacedSearchTerm = searchTerm.replace(' ', '%20');
+    url = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + replacedSearchTerm + "&api_key=a63e099b17dfc041516f78ab8c3e3d5c&format=json"
 
 
     $.getJSON(url, function (data) {
         console.log(url);
         console.log(data);
         console.log(data.artist.bio.content);
+        console.log(data.artist.image[2]);
 
     })
 }
