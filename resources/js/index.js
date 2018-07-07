7e88daa25cd277da7500b03b42b6682a81f61db2
Inna <designbyinna@gmail.com>
2018-07-07 12:37:55 -0600

    calndar and city autocomplete inputs

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,15 @@
-// Shared secret	50430e261d47ee60d575c432c912c0e5
+// for last fm Shared secret	50430e261d47ee60d575c432c912c0e5
+//band in town id 9ac9ab26c18a220660a4a733194e08fc
 
 var lstFmKey = "a63e099b17dfc041516f78ab8c3e3d5c";
 
 
 var searchTerm;
+var cityTerm;
+var startDateTerm;
+var endDateTerm;
 
+// ______________________________ LAST FM DATA API CALL _____________________________________________
 function getLastFm() {
     searchTerm = $("#search-input").val().trim();
     var replacedSearchTerm = searchTerm.replace(' ', '%20');
@@ -21,8 +26,47 @@ function getLastFm() {
 }
 
 
+$("#search-btn").on("click", function () {
+    event.preventDefault();
+    getLastFm();
+})
+
+// ______________________________ GOOGLE LOCATION AUTOCOMPLETE _____________________________________________
+
+var input = document.getElementById('city-input');
+// show only cities
+var options = {
+    types: ['(cities)'],
+};
+
+var autocompleteData = new google.maps.places.Autocomplete(input, options);
+
 
 $("#search-btn").on("click", function () {
-    
-    getLastFm();
+    event.preventDefault();
+    console.log($("#city-input").val());
+})
+
+//_________________________ CALENDAR POP UP FOR INPUT _____________________________
+$('input[name="dates-input"]').daterangepicker();
+var startGlobal;
+var endGlobal;
+
+$(function() {
+    $('input[name="calendar-pop-up"]').daterangepicker({
+      opens: 'left',
+      autoApply: true,
+      
+    }, function(start, end) {
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+      startGlobal = start.format('YYYY-MM-DD');
+      endGlobal = end.format('YYYY-MM-DD')
+    });
+  });
+  
+  
+  $("#search-btn").on("click", function () {
+    event.preventDefault();
+    console.log(startGlobal);
+    console.log(endGlobal);
 })
