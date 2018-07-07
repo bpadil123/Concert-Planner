821da69cca942891071c15a574443c580d7033e6
Inna <designbyinna@gmail.com>
2018-07-07 13:38:38 -0600

    google sign in

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,22 @@
 // for last fm Shared secret	50430e261d47ee60d575c432c912c0e5
 //band in town id 9ac9ab26c18a220660a4a733194e08fc
+  
+$(document).ready(function () {
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD68rbaMHr4XzUKdGfHbgjT6u1JMygEilw",
+    authDomain: "harmonize-dd029.firebaseapp.com",
+    databaseURL: "https://harmonize-dd029.firebaseio.com",
+    projectId: "harmonize-dd029",
+    storageBucket: "harmonize-dd029.appspot.com",
+    messagingSenderId: "419798913580"
+  };
+  firebase.initializeApp(config);
+
 
 var lstFmKey = "a63e099b17dfc041516f78ab8c3e3d5c";
 
+ 
 
 var artistTerm;
 var cityTerm;
@@ -46,11 +60,10 @@ function bandsInTown() {
     var event = searchVenue.replace(' ', '%20');
 
 
-    // var url = "https://rest.bandsintown.com" + "/artists/" + replacedSearchTerm + "/events?app_id=9ac9ab26c18a220660a4a733194e08fc" + "date=" + startGlobal + "%2C" + endGlobal
 
     // url = "https://rest.bandsintown.com/artists/sage%20francis/events?app_id=9ac9ab26c18a220660a4a733194e08fc&date=2018-01-01%2C2018-12-31";
 
-    url = "https://rest.bandsintown.com/artists/"+ replacedSearchTerm + "/events?app_id=9ac9ab26c18a220660a4a733194e08fc&";
+    url = "https://rest.bandsintown.com/artists/"+ replacedSearchTerm + "/events?app_id=9ac9ab26c18a220660a4a733194e08fc";
 
 
     $.getJSON(url, function (data) {
@@ -110,3 +123,40 @@ $("#search-btn").on("click", function () {
     console.log(startGlobal);
     console.log(endGlobal);
 })
+
+//_________ GOOGLE SIGN IN
+
+var provider = new firebase.auth.GoogleAuthProvider();
+ provider.addScope('profile');
+ provider.addScope('email');
+
+ $(document).on('click', '.signIn', function() {
+   firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    $('.content').show();
+    loggedIn();
+   
+   });
+   $(this).removeClass('signIn')
+     .addClass('signOut')
+     .html('Sign Out Of Google');
+ });
+
+ $(document).on('click', '.signOut', function () {
+   firebase.auth().signOut().then(function() {
+     $('.content').hide();
+   }, function(error) {
+     // An error happened.
+   });
+   $(this).removeClass('signOut')
+     .addClass('signIn')
+     .html('Sign In With Google');
+ });
+
+
+});
+
+ 
