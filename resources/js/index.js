b60195e45ddaa2369ab71cdd8aaf7d6cdae674ed
Inna <designbyinna@gmail.com>
2018-07-07 10:43:50 -0600

    remerge

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,9 @@
-// API key	a63e099b17dfc041516f78ab8c3e3d5c
 // Shared secret	50430e261d47ee60d575c432c912c0e5
 
 var lstFmKey = "a63e099b17dfc041516f78ab8c3e3d5c";
 
 
 var searchTerm;
-
-
-
 
 function getLastFm() {
     searchTerm = $("#search-input").val().trim();
