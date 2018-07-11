cffe339caee1edeb8998fc67595615c1dc492538
Inna <designbyinna@gmail.com>
2018-07-10 19:41:26 -0600

    filters all location to find a match, but not behavior set for now match

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,6 @@
-
-
 // for last fm Shared secret	50430e261d47ee60d575c432c912c0e5
 //band in town id 9ac9ab26c18a220660a4a733194e08fc
-  
+
 $(document).ready(function () {
     // Initialize Firebase
     var config = {
@@ -12,156 +10,237 @@ $(document).ready(function () {
         projectId: "harmonize-dd029",
         storageBucket: "harmonize-dd029.appspot.com",
         messagingSenderId: "419798913580"
-      };
-      firebase.initializeApp(config);
-    
-    
+    };
+    firebase.initializeApp(config);
+
+
     var lstFmKey = "a63e099b17dfc041516f78ab8c3e3d5c";
-    
-     
-    
+
+
+
     var artistTerm;
     var cityTerm;
-    var startGlobal;
-    var endGlobal;
-    
+    var citySelected = false;
+    // var startGlobal;
+    // var endGlobal;
+
     // ______________________________ LAST FM DATA API CALL _____________________________________________
     function getLastFm() {
         artistTerm = $("#artist-input").val().trim();
         var replacedSearchTerm = artistTerm.replace(' ', '%20');
         url = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + replacedSearchTerm + "&api_key=a63e099b17dfc041516f78ab8c3e3d5c&format=json"
-    
-    
+
+
         $.getJSON(url, function (data) {
             console.log(url);
             console.log(data);
             console.log(data.artist.bio.content);
             console.log(data.artist.image[2]);
-    
+
         })
     }
-    
-    
+
+
     $("#search-btn").on("click", function () {
         event.preventDefault();
-        getLastFm();
+        // getLastFm();
     })
-    
-    //_____________BAND IN TOWN
-    
-    
-    function bandsInTown() {
+
+    //_____________BAND IN TOWN EVENT SEARCH
+
+
+    function bandsInTownEvent() {
         artistTerm = $("#artist-input").val().trim();
         // searchVenue = $("#city-input").val().trim();
-        var replacedSearchTerm = artistTerm.replace(' ', '%20');
-        // searchStartDate = $("#dates-input").val().trim();
-        // searchEndDate = $("#end-input").val().trim();
-    
-    
-        // var artist = searchArtist.replace(' ', '%20');
+        var replacedSearchTerm = artistTerm.replace(' ', '%20') || artistTerm.replace('/', '%252F') || artistTerm.replace('?', '%253F') && artistTerm.replace('*', ' %252A') || artistTerm.replace('"', ' %27C');
+
+
+
+
         // var event = searchVenue.replace(' ', '%20');
-    
-    
-    
-        // url = "https://rest.bandsintown.com/artists/sage%20francis/events?app_id=9ac9ab26c18a220660a4a733194e08fc&date=2018-01-01%2C2018-12-31";
-    
-        url = "https://rest.bandsintown.com/artists/"+ replacedSearchTerm + "/events?app_id=9ac9ab26c18a220660a4a733194e08fc";
-    
-    
+
+
+
+
+        url = "https://rest.bandsintown.com/artists/" + replacedSearchTerm + "/events?app_id=9ac9ab26c18a220660a4a733194e08fc";
+
+
         $.getJSON(url, function (data) {
             console.log(data);
             console.log(url);
-            // console.log(url);
-            // console.log(data);
-            // console.log(data.artist.bio.content);
-            // console.log(data.artist.image[2]);
-    
-        })
-    }
-    
-    
-    
+            var eventResults = data.response
+
+            //loop to go through all results
+            for (var i = 0; i < data.length; i++) {
+                var oneResult = $("<div>");
+                oneResult.addClass("oneResult");
+                //image of artist/event from LastFM **** 
+
+
+                //name of event/artist
+                // console.log(data[i].description)
+                var eventName = $("<p>").text(data[i].description);
+
+                //name of venue
+                // console.log(data[i].venue.name);
+                var venueName = $("<p>").text(data[i].venue.name);
+
+                //city and state of venue
+                // console.log(data[i].venue.city)
+                // console.log(data[i].venue.region)
+                // var venueLocation = $("<h1>").text(data[i].venue.city + data[i].venue.region);
+
+                //date/time of event
+                // console.log(data[i].datetime) //need to use moment to convert into appropriate layout
+                eventDateTime = $("<p>").text(data[i].datetime);
+
+
+
+                //Clears previous stuff and appends new content
+
+
+                if (citySelected == true) {
+                    if (data[i].venue.city + ", " + data[i].venue.region + ", USA" == cityTerm) {
+                        console.log("it's a match");
+                        oneResult.text(data[i].venue.city + ", " + data[i].venue.region + ", USA");
+                        oneResult.append(eventName, venueName, eventDateTime);
+                        $(".searchresult").append(oneResult);
+                    } else {
+                        console.log("it's  not a match");
+                    }
+                }
+            };
+        });
+    };
+
+
+    //_____________BAND IN TOWN ARTIST SEARCH
+
+    function bandsInTownArtist() {
+        artistTerm = $("#artist-input").val().trim();
+        // searchVenue = $("#city-input").val().trim();
+        var replacedSearchTerm = artistTerm.replace(' ', '%20') || artistTerm.replace('/', '%252F') || artistTerm.replace('?', '%253F') && artistTerm.replace('*', ' %252A') || artistTerm.replace('"', ' %27C');
+
+
+
+        url = "https://rest.bandsintown.com/artists/" + replacedSearchTerm + "?app_id=9ac9ab26c18a220660a4a733194e08fc";
+
+
+        $.getJSON(url, function (data) {
+            console.log(data);
+            console.log(url);
+            var artistResults = data.response
+
+            //loop to go through all results
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i].name)
+                //image of artist/event from Artist URL **** 
+                var artistImage = $("<img>").attr("src", data[i].thumb_url);
+
+
+
+                //Clears previous stuff and appends new content
+                $(".searchresult").append(artistImage);
+            };
+        });
+    };
+
+
+
+
+
+
+
+
+    //_____________SEARCH BUTTON CLICK
+
     $("#search-btn").on("click", function () {
         event.preventDefault();
-        console.log("search was pressed");
-        bandsInTown();
+        cityTerm = $("#city-input").val().trim();
+        if (cityTerm != "") {
+            citySelected = true;
+        } else {
+            citySelcted = false;
+        }
+        // console.log("search was pressed");
+        // $(".modal").hide();
+        // $(".fade").hide();
+        $(".searchresult").empty();
+        bandsInTownArtist();
+        bandsInTownEvent();
     })
-    
+
+
+
+
+
+
+
     // ______________________________ GOOGLE LOCATION AUTOCOMPLETE _____________________________________________
-    
-    // var input = document.getElementById('city-input');
-    // // show only cities
-    // var options = {
-    //     types: ['(cities)'],
-    // };
-    
-    // var autocompleteData = new google.maps.places.Autocomplete(input, options);
-    
-    
-    // $("#search-btn").on("click", function () {
-    //     event.preventDefault();
-    //     console.log($("#city-input").val());
-    // })
-    
-    // //_________________________ CALENDAR POP UP FOR INPUT _____________________________
-    // $('input[name="dates-input"]').daterangepicker();
-    
-    
-    // $(function () {
-    //     $('input[name="calendar-pop-up"]').daterangepicker({
-    //         opens: 'left',
-    //         autoApply: true,
-    
-    //     }, function (start, end) {
-    //         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    //         startGlobal = start.format('YYYY-MM-DD');
-    //         endGlobal = end.format('YYYY-MM-DD')
-    //     });
-    // });
-    
-    
-    // $("#search-btn").on("click", function () {
-    //     event.preventDefault();
-    //     console.log(startGlobal);
-    //     console.log(endGlobal);
-    // })
-    
-    //_________ GOOGLE SIGN IN_____________________________________//
-    
-    var provider = new firebase.auth.GoogleAuthProvider();
-     provider.addScope('profile');
-     provider.addScope('email');
-    
-     $(document).on('click', '.signIn', function() {
-       firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // $('.content').show();
-        loggedIn();
-        window.location = 'https://bpadil123.github.io/Concert-Planner/search.html'
-       
-       });
-       $(this).removeClass('signIn')
-         .addClass('signOut')
-         .html('Sign Out Of Google');
-     });
-    
-     $(document).on('click', '.signOut', function () {
-       firebase.auth().signOut().then(function() {
-        //  $('.content').hide();
-       }, function(error) {
-         // An error happened.
-       });
-       $(this).removeClass('signOut')
-         .addClass('signIn')
-        //  .html('Sign In With Google');
-     });
-    
-    
-    
+
+    var input = document.getElementById('city-input');
+    // show only cities
+    var options = {
+        types: ['(cities)'],
+    };
+
+    var autocompleteData = new google.maps.places.Autocomplete(input, options);
+
+
+    $("#search-btn").on("click", function () {
+        event.preventDefault();
+        console.log($("#city-input").val());
+    })
+
+    //_________________________ CALENDAR POP UP FOR INPUT _____________________________
+    $('input[name="dates-input"]').daterangepicker();
+
+
+    $(function () {
+        $('input[name="calendar-pop-up"]').daterangepicker({
+            opens: 'left',
+            autoApply: true,
+
+        }, function (start, end) {
+            console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+            startGlobal = start.format('YYYY-MM-DD');
+            endGlobal = end.format('YYYY-MM-DD')
+        });
     });
-    
-     
-    
+
+
+    //_________ GOOGLE SIGN IN
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+
+    $(document).on('click', '.signIn', function () {
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Google Access Token.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            $('.content').show();
+            loggedIn();
+
+        });
+        $(this).removeClass('signIn')
+            .addClass('signOut')
+            .html('Sign Out Of Google');
+    });
+
+    $(document).on('click', '.signOut', function () {
+        firebase.auth().signOut().then(function () {
+            $('.content').hide();
+        }, function (error) {
+            // An error happened.
+        });
+        $(this).removeClass('signOut')
+            .addClass('signIn')
+            .html('Sign In With Google');
+    });
+
+
+
+});
