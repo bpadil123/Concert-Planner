03fb08230a8d50aac4cc3d7b1f38db46260e7d26
Inna <designbyinna@gmail.com>
2018-07-12 20:35:19 -0600

    update to sign in

--- resources/js/index.js
+++ resources/js/index.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     var artistTerm;
     var cityTerm;
     var citySelected = false;
-    // var cityMatchArr = [];
+
 
     // ______________________________ LAST FM DATA API CALL _____________________________________________
     function getLastFm() {
@@ -238,60 +238,6 @@ $(function () {
 });
 
 
-//_________ GOOGLE SIGN IN
-
-var provider = new firebase.auth.GoogleAuthProvider(); provider.addScope('profile'); provider.addScope('email');
-
-$(document).on('click', '.signIn', function () {
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        $('.content').show();
-        loggedIn();
-
-    });
-    $(this).removeClass('signIn')
-        .addClass('signOut')
-        .html('Sign Out Of Google');
-});
-
-$(document).on('click', '.signOut', function () {
-    firebase.auth().signOut().then(function () {
-        $('.content').hide();
-    }, function (error) {
-        // An error happened.
-    });
-    $(this).removeClass('signOut')
-        .addClass('signIn')
-        .html('Sign In With Google');
-});
-
-
-//MAPS//
-// Initialize and add the map
-// function initMap() {
-//     // The location of Uluru
-//     var uluru = {lat: -25.344, lng: 131.036};
-//     // The map, centered at Uluru
-//     var map = new google.maps.Map(
-//         document.getElementById('map'), {zoom: 4, center: uluru});
-//     // The marker, positioned at Uluru
-//     var marker = new google.maps.Marker({position: uluru, map: map});
-//   }
-//   <!--Load the API from the specified URL
-//     * The async attribute allows the browser to render the page while the API loads
-//     * The key parameter will contain your own API key (which is not needed for this tutorial)
-//     * The callback parameter executes the initMap() function
-//     -->
-// function myMap() {
-//     var mapProp = {
-//         center: new google.maps.LatLng(51.508742, -0.120850),
-//         zoom: 5,
-//     };
-//     var map = new google.maps.Map(document.getElementById("map"), mapProp);
-// }
 
 var map = new GMaps({
     div: '#map',
