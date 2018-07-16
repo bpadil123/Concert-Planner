cbe188964f5ed32169035d550427a9c8bd8627b3
Inna <designbyinna@gmail.com>
2018-07-16 17:36:55 -0600

    remove from database works

--- resources/js/index-favorites.js
+++ resources/js/index-favorites.js
@@ -1,3 +1,4 @@
+var oneFaveId;
 $(document).ready(function () {
   // Initialize Firebase
 
@@ -10,7 +11,7 @@ $(document).ready(function () {
       // User is signed in.
       var displayName = user.displayName;
 
-    
+
       var globalUserId = user.uid;
       console.log(displayName);
       $(".user-name").text(displayName + "'s Favorite Concerts");
@@ -20,39 +21,38 @@ $(document).ready(function () {
         //  console.log(childSnapshot.val().event);
         //  console.log(childSnapshot.val().name);
         //  console.log(childSnapshot.val().time);
-         console.log(childSnapshot.val().ticket);
+        //  console.log(childSnapshot.val().ticket);
+        console.log(childSnapshot.val().id)
+        //  oneFaveId = childSnapshot.val().id;
+        //  console.log(oneFaveId);
         // append to our table of favorites, inside tbody, with a new row of the data
         $("#data-favorites").append(
-          "<i id='remove' class='fas fa-times' onclick= {console.log('hello')}></i><h3>" + childSnapshot.val().name + "</h3>" +
+          "<i class='fas fa-times remove' data-id=" + childSnapshot.val().id + "></i><h3>" + childSnapshot.val().name + "</h3>" +
           "<div class='time'>" + childSnapshot.val().time + "</div>" +
           "<div class='venue'>" + childSnapshot.val().venue + "</div>" +
-          "<div class='city'>" + childSnapshot.val().city + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</div>" + 
-          "<a target='_blank' href='" + childSnapshot.val().ticket +"'><i id='ticket-fav' class='fas fa-ticket-alt'></>" + "</a><hr>"
+          "<div class='city'>" + childSnapshot.val().city + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</div>" +
+          "<a target='_blank' href='" + childSnapshot.val().ticket + "'><i id='ticket-fav' class='fas fa-ticket-alt'></>" + "</a><hr>"
         );
       })
 
-      $(document).on('click', 'remove', function () {
-        console.log ('clickerspickers')
-        debugger
-        name = $(this).data("name");
-        venue = $(this).data("venue");
-        time = $(this).data("time");
-        event = $(this).data("event");
-        city = $(this).data("city");
-        ticket = $(this).data("ticket");
-        data = {
-            "name": name,
-            "venue": venue,
-            "time": time,
-            "event": event,
-            "city": city,
-            "ticket": ticket
-        };
-        //debugger
-        fb_db.child(globalUserId).remove(data)
-    });
+      $(document).on('click', '.remove', function () {
+
+        oneFaveId = $(this).data("id");
+        deleteRecord(oneFaveId);
+      });
       // append to our table of favorites, inside tbody, with a new row of the data
       // debugger
+      function deleteRecord(key) {
+        var refDB = firebase.database().ref(globalUserId + "/" + oneFaveId);
+
+        refDB.remove()
+          .then(function () {
+            console.log("Remove succeeded.")
+          })
+          .catch(function (error) {
+            console.log("Remove failed: " + error.message)
+          });
+      }
 
 
       //database.ref().on("child_added", function (childSnapshot) {
@@ -62,6 +62,10 @@ $(document).ready(function () {
       // ...
     }
   });
+
+  function displayResults(){
+    
+  }
 
 
 });
